2a91d02d437bb4533e9e404b4bec032dbd70fcaf
Bruce <1178334271@qq.com>
2020-01-31 14:57:14 +0800

    start to build calculator

---
+++ calculate.h
@@ -0,0 +1,8 @@
+//
+// Created by Bruce on 2020/1/31.
+//
+
+#ifndef CASIO_CALCULATE_H
+#define CASIO_CALCULATE_H
+
+#endif //CASIO_CALCULATE_H
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "What a nice vacation!" << endl;
-    return 0;
-}
