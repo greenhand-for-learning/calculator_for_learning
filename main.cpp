0101b57de497ac9176f282a3ccbe9ba20e9205c8
huangb19 <444575947@qq.com>
2020-02-02 13:13:35 +0800

    add statistic

--- main.cpp
+++ main.cpp
@@ -1,15 +1,46 @@
 #include "calculate.h"
+#include "statistic.h"
 #include <iostream>
 #include <string>
 using namespace std;
 int main(){
-    string tmp;
-    while(getline(cin, tmp)){
-        bool valid = true;
-        double ans = calculate_with_real(tmp, valid, "D");
-        if(valid){
-            cout << tmp << " = " << ans << endl;
+    int opt;
+    cin >> opt;
+    if(opt == 1){
+        string tmp;
+        while(getline(cin, tmp)) {
+            bool valid = true;
+            double ans = calculate_with_real(tmp, valid, "D");
+            if (valid) {
+                cout << tmp << " = " << ans << endl;
+            }
         }
     }
+    else if(opt == 2){
+
+    }
+    else if(opt == 3){
+
+    }
+    else if(opt == 4){
+
+    }
+    else if(opt == 5){
+
+    }
+    else if(opt == 6){
+        statistic_main();
+    }
+    else if(opt == 7){
+
+    }
+    else if(opt == 8){
+
+    }
+    else if(opt == 9){
+
+    }
+
+
     return 0;
 }
---
+++ statistic.cpp
@@ -0,0 +1,17 @@
+//
+// Created by huangb19 on 2020/2/2.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+void statistic_main(){
+    cout << "hello!";
+
+    return;
+}
+
+
+
